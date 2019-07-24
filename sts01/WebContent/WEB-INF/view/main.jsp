92c516f8ffd83b2b1cdf19369c7c7ea29d93190c
Song Hae <thdgo456@naver.com>
2019-07-24 17:38:29 +0900

    sts01 - ModelAndView를 이용한 페이지 매핑, msg 전송

---
+++ sts01/WebContent/WEB-INF/view/main.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=EUC-KR"
+    pageEncoding="utf-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<h1>Main Page(main.jsp)</h1>
+	<p>msg param:${msg }</p>
+
+</body>
+</html>
---
+++ sts01/src/com/bit/controller/MainController.java
@@ -0,0 +1,23 @@
+package com.bit.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class MainController implements Controller {
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		//View에 보낼 model과 View를 모두 지닌 클래스
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("main");
+		
+		//msg를 보내는 방법 - 내부적으로는 같다.
+//		request.setAttribute("msg", "환영합니다.");
+		mav.addObject("msg","환영!");
+		return mav;
+	}
+
+}
