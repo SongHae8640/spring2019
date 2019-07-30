1dd0e266d78363ba5ed4235eea5a5dced259deb5
Song Hae <thdgo456@naver.com>
2019-07-30 10:29:14 +0900

    sts06 - 환경설정, 어노테인션 시작
    수업.txt에 변경된 환경 설정 내용 추가
    ex01, ex02를 통해 어노테이션 개념 추가

---
+++ sts06/src/main/java/com/bit/day05/Ex02Controller.java
@@ -0,0 +1,14 @@
+package com.bit.day05;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class Ex02Controller {
+
+	@RequestMapping("/ex02")
+	public String mapping() {
+		return "ex02";
+	}
+}
