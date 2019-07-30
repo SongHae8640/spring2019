b5213b7b8991006f1989c67af8819fa6c3bc923d
Song Hae <thdgo456@naver.com>
2019-07-30 14:19:30 +0900

    sts06 - 혼자 연습하는 BBS :: ListController 약간
    갑자기 수업 진행해서 수업으로 go

---
+++ sts06/src/main/java/com/bit/day05/BbsListController.java
@@ -0,0 +1,24 @@
+package com.bit.day05;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class BbsListController implements org.springframework.web.servlet.mvc.Controller{
+
+
+	@Override
+	@RequestMapping(value= {"/bbs/list","/bbs/","/bbs"})
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		// TODO Auto-generated method stub
+		
+		ModelAndView mav = new ModelAndView();
+//		mav.addObject("", attributeValue)
+		mav.setViewName("bbs/list");
+		return mav;
+	}
+}
--- sts06/src/main/webapp/WEB-INF/views/home.jsp
+++ sts06/src/main/webapp/WEB-INF/views/home.jsp
@@ -12,5 +12,6 @@
 </h1>
 
 <P>  The time on the server is ${serverTime}. </P>
+<a href="bbs">BBS 입장</a>
 </body>
 </html>
